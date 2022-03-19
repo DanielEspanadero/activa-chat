1928bca90a474342d95e12a924549735e066c1a7
Daniel Españadero <developer@daniel-espanadero.com>
2022-03-19 17:07:09 +0100

    user schema and model created

--- server/dist/models/user.js
+++ server/dist/models/user.js
@@ -1,2 +1,25 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.User = void 0;
+const mongoose_1 = require("mongoose");
+const UserSchema = new mongoose_1.Schema({
+    firstName: {
+        type: String,
+        required: [true, 'First name is required.']
+    },
+    lastName: {
+        type: String,
+        required: [true, 'Last name is required.']
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: [true, 'Email is required.']
+    },
+    avatar: {
+        type: String,
+        default: 'https://github.com/DanielEspanadero/activa-chat/blob/master/client/public/assets/img/man.png'
+    }
+});
+exports.User = (0, mongoose_1.model)('User', UserSchema);
 //# sourceMappingURL=user.js.map
---
+++ server/src/models/user.ts
@@ -0,0 +1,23 @@
+import {Schema, model} from 'mongoose';
+
+const UserSchema: any = new Schema({
+    firstName: {
+        type: String,
+        required: [true, 'First name is required.']
+    },
+    lastName: {
+        type: String,
+        required: [true, 'Last name is required.']
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: [true, 'Email is required.']
+    },
+    avatar: {
+        type: String,
+        default: 'https://github.com/DanielEspanadero/activa-chat/blob/master/client/public/assets/img/man.png'
+    }
+});
+
+export const User = model('User', UserSchema)
