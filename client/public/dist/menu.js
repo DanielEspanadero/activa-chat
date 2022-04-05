8495c6ceb9d7bc1a538deb607a5957e695fd7b46
Daniel Españadero <developer@daniel-espanadero.com>
2022-04-05 10:28:45 +0200

    responsive chat improved

--- client/public/dist/menu.js
+++ client/public/dist/menu.js
@@ -6,15 +6,30 @@
         const users = document.querySelector('#button-users');
         const usersContainer = document.querySelector('.hidden-users');
         const chatContainer = document.querySelector('.hidden-chat');
+        const closeRooms = document.querySelector('.close-rooms');
+        const closeUsers = document.querySelector('.close-users');
         rooms.addEventListener('click', () => {
-            if (window.innerWidth < 768) {
+            if (window.innerWidth < 1000) {
                 roomsContainer.classList.toggle('hidden-rooms');
                 chatContainer.classList.toggle('hidden-chat');
             }
             ;
         });
+        closeRooms.addEventListener('click', () => {
+            if (window.innerWidth < 1000) {
+                roomsContainer.classList.toggle('hidden-rooms');
+                chatContainer.classList.toggle('hidden-chat');
+            }
+        });
         users.addEventListener('click', () => {
-            if (window.innerWidth < 768) {
+            if (window.innerWidth < 1000) {
+                usersContainer.classList.toggle('hidden-users');
+                chatContainer.classList.toggle('hidden-chat');
+            }
+            ;
+        });
+        closeUsers.addEventListener('click', () => {
+            if (window.innerWidth < 1000) {
                 usersContainer.classList.toggle('hidden-users');
                 chatContainer.classList.toggle('hidden-chat');
             }
--- client/public/ts/menu.ts
+++ client/public/ts/menu.ts
@@ -5,18 +5,36 @@
         const users: HTMLDivElement = document.querySelector('#button-users')!;
         const usersContainer: HTMLDivElement = document.querySelector('.hidden-users')!;
         const chatContainer: HTMLElement = document.querySelector('.hidden-chat')!;
+        const closeRooms: HTMLDivElement = document.querySelector('.close-rooms')!;
+        const closeUsers: HTMLDivElement = document.querySelector('.close-users')!;
 
         rooms.addEventListener('click', () => {
-            if (window.innerWidth < 768) {
+            if (window.innerWidth < 1000) {
                 roomsContainer.classList.toggle('hidden-rooms');
                 chatContainer.classList.toggle('hidden-chat');
             };
         });
+
+        closeRooms.addEventListener('click', () => {
+            if (window.innerWidth < 1000) {
+                roomsContainer.classList.toggle('hidden-rooms');
+                chatContainer.classList.toggle('hidden-chat');
+            }
+        });
+
         users.addEventListener('click', () => {
-            if (window.innerWidth < 768) {
+            if (window.innerWidth < 1000) {
                 usersContainer.classList.toggle('hidden-users');
                 chatContainer.classList.toggle('hidden-chat');
             };
         });
+
+        closeUsers.addEventListener('click', () => {
+            if (window.innerWidth < 1000) {
+                usersContainer.classList.toggle('hidden-users');
+                chatContainer.classList.toggle('hidden-chat');
+            };
+        }); 
+
     });
 })()
